c7fe1371594bb09f31296ee1e5cc3441642750ce
AidanYok <ayokuda@aol.com>
2023-04-30 16:27:10 -0700

    added CORS middleware back to index.js

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
