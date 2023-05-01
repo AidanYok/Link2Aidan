c3061045afcf8bb6cd558229b33282ea3f9e5c66
AidanYok <ayokuda@aol.com>
2023-04-30 19:22:33 -0700

    realized that GPT3.5 removed the vercel-spec CORS initilization for node.js, put in and current still have gpt3.5 cors init

--- index.js
+++ index.js
@@ -4,6 +4,50 @@ const dataRouter = require('./api/data');
 const getLinksRouter = require('./api/get-links');
 const submitLinksRouter = require('./api/submit-links');
 
+const allowCors = fn => async (req, res) => {
+
+  res.setHeader('Access-Control-Allow-Credentials', true)
+
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
+  // another common pattern
+
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+
+  res.setHeader(
+
+    'Access-Control-Allow-Headers',
+
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+
+  )
+
+  if (req.method === 'OPTIONS') {
+
+    res.status(200).end()
+
+    return
+
+  }
+
+  return await fn(req, res)
+
+}
+
+
+const handler = (req, res) => {
+
+  const d = new Date()
+
+  res.end(d.toString())
+
+}
+
+
+module.exports = allowCors(handler)
+
 const server = http.createServer(async (req, res) => {
   const { pathname } = url.parse(req.url);
 
