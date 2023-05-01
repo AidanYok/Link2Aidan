19dca4342f2d3e9a2039d23cbdd77ff8c2962eb3
AidanYok <ayokuda@aol.com>
2023-04-30 17:44:11 -0700

    init basic express.js CORS imp

--- index.js
+++ index.js
@@ -1,55 +1,11 @@
-require('dotenv').config();
-
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const path = require('path');
-const app = express();
-
-app.use(express.json());
-
-const PORT = process.env.PORT || 3000;
-
-const uri = `mongodb+srv://ayokuda:${process.env.MONGO_PASSWORD}@cluster0.fmquarj.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const clientSideDirectory = 'public';
-app.use(express.static(path.join(__dirname, clientSideDirectory)));
-
-app.post('/submit-link', async (req, res) => {
-    const { name, link } = req.body;
-
-    try {
-        await client.connect();
-        const collection = client.db('your_database').collection('links');
-        await collection.insertOne({ name, link });
-        res.status(201).json({ message: 'Link submitted successfully!' });
-    } catch (err) {
-        res.status(500).json({ message: 'Error submitting the link. Please try again.' });
-    } finally {
-        await client.close();
-    }
-});
-
-app.get('/get-links', async (req, res) => {
-    try {
-        await client.connect();
-        const collection = client.db('your_database').collection('links');
-        const links = await collection.find({}).toArray();
-        res.json(links);
-    } catch (err) {
-        res.status(500).json({ message: 'Error retrieving links. Please try again.' });
-    } finally {
-        await client.close();
-    }
-});
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, clientSideDirectory, 'index.html'));
-});
+  res.send('Hello, World!')
+})
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
-module.exports = app;
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running...')
+})
 
