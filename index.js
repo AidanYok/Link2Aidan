18364732100f8af54467adc733ff7da5e23b1197
AidanYok <ayokuda@aol.com>
2023-04-30 19:24:02 -0700

    removed GPT3.5 CORS init

--- index.js
+++ index.js
@@ -51,21 +51,6 @@ module.exports = allowCors(handler)
 const server = http.createServer(async (req, res) => {
   const { pathname } = url.parse(req.url);
 
-  // CORS headers
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-
-  if (req.method === 'OPTIONS') {
-    res.writeHead(200);
-    res.end();
-    return;
-  }
-
   if (pathname === '/api/data') {
     return dataRouter(req, res);
   }
