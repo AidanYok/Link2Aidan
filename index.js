7e05e338e293cc3be6a2772509f1c00b6dd578d0
AidanYok <ayokuda@aol.com>
2023-04-30 19:09:56 -0700

    basic node.js project with CORS working and now implemted link2-aidan code

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
+// node.js
 const http = require('http')
 const url = require('url')
 const dataRouter = require('./api/data')
+
+const { MongoClient } = require('mongodb');
+const path = require('path');
 
 const server = http.createServer(async (req, res) => {
   const { pathname } = url.parse(req.url)
@@ -33,8 +39,49 @@ const server = http.createServer(async (req, res) => {
   }
 })
 
-const port = process.env.PORT || 3000
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+const PORT = process.env.PORT || 3000;
+
+const uri = `mongodb+srv://ayokuda:${process.env.MONGO_PASSWORD}@cluster0.fmquarj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const clientSideDirectory = 'client/';
+app.use(express.static(path.join(__dirname, clientSideDirectory)));
+
+app.post('/submit-link', async (req, res) => {
+    const { name, link } = req.body;
+
+    try {
+        await client.connect();
+        const collection = client.db('your_database').collection('links');
+        await collection.insertOne({ name, link });
+        res.status(201).json({ message: 'Link submitted successfully!' });
+    } catch (err) {
+        res.status(500).json({ message: 'Error submitting the link. Please try again.' });
+    } finally {
+        await client.close();
+    }
+});
+
+app.get('/get-links', async (req, res) => {
+    try {
+        await client.connect();
+        const collection = client.db('your_database').collection('links');
+        const links = await collection.find({}).toArray();
+        res.json(links);
+    } catch (err) {
+        res.status(500).json({ message: 'Error retrieving links. Please try again.' });
+    } finally {
+        await client.close();
+    }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, clientSideDirectory, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
 
